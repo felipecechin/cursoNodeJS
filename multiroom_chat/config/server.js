b942783a3759ba588699d0a00dfebd4bea04e268
Felipe Cechin Mello <ficechin@hotmail.com>
2017-08-30 22:00:08 -0300

    Ajustes no projeto portal de notícias

---
+++ multiroom_chat/config/server.js
@@ -0,0 +1,16 @@
+//importar módulo do framework express
+var express = require('express');
+//importar módulo do consign
+var consign = require('consign');
+//importar módulo body-parser
+var bodyParser = require('body-parser');
+//importar módulo do express-validator
+var expressValidator = require('express-validator');
+//iniciar o objeto do express
+var app = express();
+
+//
+
+
+//exportar o objeto app
+module.exports = app;
