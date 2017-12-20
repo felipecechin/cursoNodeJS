a088e41dd311e45d0d119c75ec552df776fa40d4
Felipe Cechin Mello <ficechin@hotmail.com>
2017-12-19 23:28:35 -0200

    Seção 17 do curso concluída com sucesso.

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -2,9 +2,9 @@ function UsuariosDAO(connection){
     this._connection = connection();
 }
 
-UsuariosDAO.prototype.inserirUsuario = function(usuario) {
-    this._connection.open( function(err, mongoclient) {
-        mongoclient.collection("usuarios", function(err, collection) {
+UsuariosDAO.prototype.inserirUsuario = function(usuario){
+    this._connection.open( function(err, mongoclient){
+        mongoclient.collection("usuarios", function(err, collection){
             collection.insert(usuario);
 
             mongoclient.close();
@@ -12,6 +12,36 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario) {
     });
 }
 
-module.exports = function() {
+
+UsuariosDAO.prototype.autenticar = function(usuario, req, res) {
+    this._connection.open( function(err, mongoclient){
+        mongoclient.collection("usuarios", function(err, collection){
+            collection.find(usuario).toArray(function(err, result) {
+                if (result[0] != undefined) {
+                    req.session.autorizado = true;
+                    req.session.usuario = result[0].usuario;
+                    req.session.casa = result[0].casa;
+                }
+
+                if (req.session.autorizado) {
+                    res.redirect("jogo");
+                } else {
+                    res.render("index",{validacao:
+                        [
+                        {param: 'usuario',
+                        msg: 'Usuário e ou senha inválidos',
+                        value: usuario.usuario}
+                        ]
+                    });
+                }
+            });
+            //ou {usuario: {$eq: usuario.usuario}, senha {$eq: usuario.senha}}
+
+            mongoclient.close();
+        });
+    });
+}
+
+module.exports = function(){
     return UsuariosDAO;
 }
