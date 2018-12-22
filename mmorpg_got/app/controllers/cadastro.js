2c54cd195cb830db38b5deda3346cc62093a5c68
Felipe Cechin Mello <ficechin@hotmail.com>
2018-12-22 03:19:48 -0200

    Ajuste no projeto de mmorpg_got.

--- mmorpg_got/app/controllers/cadastro.js
+++ mmorpg_got/app/controllers/cadastro.js
@@ -26,5 +26,5 @@ module.exports.cadastrar = function(application, req, res) {
     JogoDAO.gerarParametros(dadosForm.usuario);
     //geração dos parametros
 
-    res.send("podemos");
+    res.redirect("/");
 }
