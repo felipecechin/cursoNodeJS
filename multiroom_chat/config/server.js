d6294dec859bdb74a7f32bf1638768e4de9a1a60
Felipe Cechin Mello <ficechin@hotmail.com>
2018-12-22 02:32:12 -0200

    Ajuste no projeto multiroom chat.

--- multiroom_chat/config/server.js
+++ multiroom_chat/config/server.js
@@ -23,7 +23,6 @@ app.use(expressValidator());
 //efetua o autoload das rotas, dos models e dos controllers para o objeto app
 consign()
 	.include('app/routes')
-	.then('app/models')
 	.then('app/controllers')
 	.into(app);
 
